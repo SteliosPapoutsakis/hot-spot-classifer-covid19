98dd3f7fe9ab613ed3928ba107b8cafda2bac197
murphyp <murphy.p@northeastern.edu>
2020-10-17 21:16:02 -0400

    counties key by ID number

--- county_relations.py
+++ county_relations.py
@@ -17,3 +17,12 @@ class County:
     def add_neighbor(self, neighbor):
         self.neighbors.append(neighbor)
 
+def create_county_key(counties):
+    alphabetical_counties = sorted(counties, key=lambda x: x.name)
+    county_key = {}
+    for i in range(len(alphabetical_counties)):
+        county_id = alphabetical_counties[i].id
+        county_key[county_id] = (i, alphabetical_counties[i])
+    
+    print(county_key)
+    return county_key
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from parse_utils import parse_county_adj
-from county_relations import County
+from county_relations import County, create_county_key
 
 counties = []
 
 counties = parse_county_adj('./data/california_counties.txt') 
 
 #sory by alphabetical
+create_county_key(counties)
